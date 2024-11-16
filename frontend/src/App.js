4d66096db8538424b638a801e576258f8a512ba5
kazukoix <kazukoix>
2024-11-16 18:42:56 +0800

    Added routes that the user will be directed to on the react js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import {
-  createBrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shoes from "./pages/Shoes";
+import Add from "./pages/AddingInsideShoes";
+import Update from "./pages/UpdatingInsideShoes";
 
 function App() {
   return (
     <div className="App">
-      <createBrowserRouter>
-      </createBrowserRouter>
+      {/*BrowserRouter uses html5 history. It has functions like push, 
+      replace, pubstate to keep our UI insych with the URL .*/}
+      <BrowserRouter>
+        {/*Inside here is where we will create routes */}
+
+        <Routes>
+          {/*The elements insides of each jsx route. */}
+          <Route path = "/" element = {<Shoes/>}> </Route>
+          <Route path = "/add" element = {<Add/>}> </Route>
+          <Route path = "/update" element = {<Update/>}> </Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
